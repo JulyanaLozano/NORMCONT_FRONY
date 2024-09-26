a7015fadb95c601d07ee854a0374a609ac181a81
JulyanaLozano <josephtemi@hotmail.com>
2024-09-26 17:57:34 -0500

    Se incluye login y modulo de administracion de ususarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserAppComponent } from './components/user-app.component';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, UserAppComponent],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = '5-user-app';
+  title = 'user-app';
+  faCoffee = faCoffee;
 }
---
+++ src/app/models/user.ts
@@ -0,0 +1,14 @@
+
+export class User {
+
+    idUsuario: number = 0;
+    nombreUsuario!: string;
+    estadoUsuario: number =1;
+    apellidoUsuario!: string;
+    email!: string;
+    codUsuario!: string;
+    password!: string;
+    identificacion!: string;
+    tipoIdentificacion!:string;
+    
+}
